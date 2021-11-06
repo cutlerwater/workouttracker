f6d9c1e323f20c09d4ef626d300dbc9f1636122e
cutlerwater <cutlerwater2@live.com>
2021-11-06 12:08:10 -0400

    finished project

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,7 +3,7 @@
     <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
       <div class="flex items-center gap-x-4">
         <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
-        <h1 class="text-lg">Active Tracker</h1>
+        <h1 class="text-lg">Workout Tracker</h1>
       </div>
       <ul class="flex flex-1 justify-end gap-x-10">
         <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
